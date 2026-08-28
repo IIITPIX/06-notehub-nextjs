4eb8c06b351b6ce54e481d45889138cbf1ea3f0b
Andrii_Belmeha <iiitpix@hotmail.com>
2026-08-28 18:42:13 +0200

    fixed weong way to types

--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import type { Note } from "/Projects/06-notehub-nextjs/types/note";
+import type { Note } from "@/types/note";
 import css from "./NoteList.module.css";
 import { fetchDeleteNote } from "@/lib/api";
 import Link from "next/link";
